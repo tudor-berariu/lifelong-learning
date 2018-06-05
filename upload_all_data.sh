09ad32037c7d82516c6241464844f3b6697dcc71
Alexandru Dinu <alex.dinu07@gmail.com>
2018-06-05 23:42:39 +0300

    Improvement :))

--- upload_all_data.sh
+++ upload_all_data.sh
@@ -1,6 +1,6 @@
 
-for file in ../lifelong_tmp_data/*.pkl; do
+for file in $1*.pkl; do
     name=${file##*/}
     base=${name%.txt}
-    python utils/upload_to_elasticsearch.py "$file"
+    python upload_to_elasticsearch.py "$file"
 done
