09480c87ea76a5a9d98bac4e16f71b3b1d1e631b
andreicnica <andreic.nica@gmail.com>
2018-06-09 18:04:02 +0300

    Check if file has requested keys.

--- repair_reporting_data.py
+++ repair_reporting_data.py
@@ -16,28 +16,35 @@ if __name__ == "__main__":
         print(f"Working on: {file_path}")
 
         data = torch.load(file_path)
-        eval_seen = sorted(data["_eval_trace"].keys())
+
+        fix = False
 
         # Repair Last eval
-        data["_last_eval"] = {k: v[-1] for k, v in data["_eval_trace"][eval_seen[-1]].items()}
+        if "_eval_trace" in data:
+            eval_seen = sorted(data["_eval_trace"].keys())
+            data["_last_eval"] = {k: v[-1] for k, v in data["_eval_trace"][eval_seen[-1]].items()}
 
-        # Repair best eval
-        best_eval = dict({task["idx"]: {"acc": {"value": -1, "seen": -1},
-                                           "loss": {"value": np.inf, "seen": -1}}
-                                for task in data["_task_info"]})
-        last_eval = dict({task["idx"]: {"acc": -1, "seen": -1, "loss":  np.inf}
-                                for task in data["_task_info"]})
+            # Repair best eval
+            best_eval = dict({task["idx"]: {"acc": {"value": -1, "seen": -1},
+                                               "loss": {"value": np.inf, "seen": -1}}
+                                    for task in data["_task_info"]})
+            last_eval = dict({task["idx"]: {"acc": -1, "seen": -1, "loss":  np.inf}
+                                    for task in data["_task_info"]})
 
-        for ix in eval_seen:
-            for task_idx in data["_eval_trace"][ix].keys():
-                for eval in data["_eval_trace"][ix][task_idx]:
-                    Reporting.update_best(eval, last_eval, best_eval, task_idx, ix)
+            for ix in eval_seen:
+                for task_idx in data["_eval_trace"][ix].keys():
+                    for eval in data["_eval_trace"][ix][task_idx]:
+                        Reporting.update_best(eval, last_eval, best_eval, task_idx, ix)
 
-        data["_best_eval"] = best_eval
+            data["_best_eval"] = best_eval
+            fix = True
+        else:
+            print("Does not have key: _eval_trace")
 
-        torch.save(data, file_path)
+        if fix:
+            torch.save(data, file_path)
 
-        print(f"Done", "\n")
+        print(f"Done (Fix: {fix})", "\n")
 
 
 
