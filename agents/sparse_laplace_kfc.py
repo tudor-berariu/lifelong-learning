Fix hessian vector product.

agents/sparse_laplace_kfc.py
@@ -25,18 +25,20 @@ class GaussianPrior(Prior):
                  diag_adjust: float = 0.) -> None:
         self.mode = {name: param.clone().detach_() for (name, param) in mode}
         self.kfhp = kfhp
-        self.diag_adjust = 0.
+        self.diag_adjust = diag_adjust
 
     def __call__(self, vector: Iterator[Tuple[str, Tensor]]) -> Tensor:
-        diff2 = dict({})
-        a, count_no = None, 0
+        all_diffs = dict({})
+        diag_loss = None
         for name, values in vector:
-            diff = values - self.mode[name]
-            diff2[name] = diff * diff
-            a = diff2[name].sum() if a is None else (a + diff2[name].sum())
-            count_no += values.nelement()
-        # a /= count_no
-        return self.kfhp.hessian_product_loss(diff2) + self.diag_adjust * a
+            all_diffs[name] = diff = values - self.mode[name]
+            if self.diag_adjust > 0:
+                diag_extra = torch.dot(diff.view(-1), diff.view(-1))
+                diag_loss = diag_extra if diag_extra is None else (diag_loss + diag_extra)
+        vHv = self.kfhp.hessian_product_loss(all_diffs)
+        if self.diag_adjust > 0:
+            return vHv + self.diag_adjust * diag_loss
+        return vHv
 
 
 class SparsityPrior(Prior):
@@ -62,7 +64,7 @@ class SparseKFLaplace(BaseAgent):
         self.merge_elasticities = agent_args.merge_elasticities  # TODO: not used yet
         self.nll_scale = agent_args.nll_scale
         self.sparsity_p_norm: float = agent_args.sparsity_p_norm
-        self.laplace_scale: float = agent_args.laplace_scale
+        self.prior_scale: float = agent_args.prior_scale
         self.sparsity_scale: float = agent_args.sparsity_scale
         self.diag_adjust: float = agent_args.diag_adjust  # (H + λI)
         self.use_exact: bool = agent_args.use_exact
@@ -83,27 +85,32 @@ class SparseKFLaplace(BaseAgent):
         task_no = self.crt_task_idx
 
         losses = dict({})
-        loss = outputs[0].new_zeros(1)
+        nll_loss = outputs[0].new_zeros(1)
         for out, target in zip(outputs, targets):
-            loss += functional.cross_entropy(out, target)
-        loss *= self.nll_scale
+            nll_loss += functional.cross_entropy(out, target)
 
-        sparsity_loss = loss.new_zeros(1)
+        sparsity_loss = nll_loss.new_zeros(1)
         if self.sparsity_scale > 0:
-            sparsity_loss += self.sparsity_scale * self.sparsity_prior(model.named_parameters())
+            sparsity_loss += self.sparsity_prior(model.named_parameters())
 
-        prior_loss = loss.new_zeros(1)
+        prior_loss = nll_loss.new_zeros(1)
         if task_no > 0:
             for _idx, prior in enumerate(self.priors):
-                prior_loss += self.laplace_scale * prior(model.named_parameters())
-        losses = {
-            "nll loss": loss.item(),
-            "sparsity p-norm": sparsity_loss.item(),
-            "Gaussian prior": prior_loss.item()
-        }
-        loss += prior_loss + sparsity_loss
+                prior_loss += prior(model.named_parameters())
+        loss = self.prior_scale * prior_loss +\
+            self.sparsity_scale * sparsity_loss +\
+            self.nll_scale * nll_loss
+        loss /= self.nll_scale   # This is not to search for new hyperparameter values
 
-        loss /= self.nll_scale
+        losses = {
+            "nll loss (raw)": nll_loss.item(),
+            "sparsity p-norm (raw)": sparsity_loss.item(),
+            "Gaussian prior (raw)": prior_loss.item(),
+            "nll loss (scaled)": nll_loss.item() * self.nll_scale,
+            "sparsity p-norm (scaled)": sparsity_loss.item() * self.sparsity_scale,
+            "Gaussian prior (scaled)": prior_loss.item() * self.prior_scale,
+            "FINAL LOSS:": loss.item()
+        }
         loss.backward()
         self._optimizer.step()
         return outputs, loss, losses
@@ -114,7 +121,6 @@ class SparseKFLaplace(BaseAgent):
         self._optimizer.zero_grad()
         model = self._model
         model.use_exact = False
-        model.clamp_vector = self.clamp_vector
         model.do_kf = True
         for _batch_idx, (data, targets, head_idx) in enumerate(train_loader):
             model.zero_grad()

models/kf.py
@@ -46,13 +46,11 @@ class KFHessianProduct(object):
 
     def __init__(self,
                  inputs_cov: Dict[str, List[Tensor]],
-                 outputs_hess: Dict[str, List[Tensor]],
-                 clamp_vector: bool = False):
+                 outputs_hess: Dict[str, List[Tensor]]):
 
         if len(inputs_cov) != len(outputs_hess):
             raise ValueError("Lists should be equal in length.")
 
-        self.clamp_vector = clamp_vector  # clamp values in Hv
         self.factors = dict({})  # type: Dict[str, List[Tuple[Tensor, Tensor]]]
         for module_name, i_cov_lst in inputs_cov.items():
             o_hess_lst = outputs_hess[module_name]
@@ -65,13 +63,12 @@ class KFHessianProduct(object):
         weight, bias = weight.view(out_no, -1), bias.view(out_no, 1)
         params = torch.cat([weight, bias], dim=1)
         loss, prods_no = params.new_zeros(1), 0
-        for i_cov, o_hess in self.factors[module_name]:
-            if self.clamp_vector:
-                loss += (o_hess @ params @ i_cov).clamp(min=0).sum()
-            else:
-                loss += (o_hess @ params @ i_cov).sum()
-            prods_no += 1
-        loss /= prods_no
+        loss = torch.dot(
+            params.view(-1),
+            sum([(o_hess @ params @ i_cov) for (i_cov, o_hess) in self.factors[module_name]]).view(-1)
+        )
+        loss /= len(self.factors)
+
         return loss
 
     def hessian_product_loss(self, vector: Dict[str, Tensor]) -> None:
@@ -100,8 +97,7 @@ class KroneckerFactored(nn.Module):
                  use_fisher: bool=True,  # Fallback to Fisher for unsupported architectures
                  use_exact: bool=False,  # Use exact block Hessian when available
                  verbose: bool=False,    # Print info
-                 average_factors: bool=True,
-                 clamp_vector: bool=False) -> None:  # Average factors E(H) = E(aaT) kf E(ggT)
+                 average_factors: bool=True) -> None:  # Average factors E(H) = E(aaT) kf E(ggT)
         super(KroneckerFactored, self).__init__()
         self.__my_handles = []
         self.__kf_mode = False  # One must activate this
@@ -110,7 +106,6 @@ class KroneckerFactored(nn.Module):
         self.__do_checks = do_checks
         self.__verbose = verbose
         self.__average_factors = average_factors
-        self.clamp_vector = clamp_vector
 
         # Per KF computation (persistent over several fwd+bwd's)
         self.__outputs_hess = dict({})  # type: Dict[str, List[Tensor]]
@@ -515,7 +510,6 @@ class KroneckerFactored(nn.Module):
             for tensors in self.__outputs_hess.values():
                 assert len(tensors) == 1
                 tensors[0].mul_(coeff)
-        kfhp = KFHessianProduct(self.__inputs_cov, self.__outputs_hess,
-                                clamp_vector=self.clamp_vector)
+        kfhp = KFHessianProduct(self.__inputs_cov, self.__outputs_hess)
         self.do_kf = False  # Do not put above as this drops tensors
         return kfhp