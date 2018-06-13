2865e7035c97b7989dde57898a06dc8078458f52
Tudor BERARIU <tudor.berariu@gmail.com>
2018-06-14 01:24:18 +0300

    Add KF for convs.

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,20 @@
 from .mlp import MLP
 from .lenet import LeNet
 from .kf_mlp import KFMLP
+from .kf_lenet import KFLeNet
 
 ALL_MODELS = {
     "mlp": MLP,
     "lenet": LeNet,
-    "kf_mlp": KFMLP
+    "kf_mlp": KFMLP,
+    "kf_lenet": KFLeNet
 }
 
 ALL_MODELS_BASE_TYPE = {
     "mlp": "mlp",
     "lenet": "lenet",
-    "kf_mlp": "mlp"
+    "kf_mlp": "mlp",
+    "kf_lenet": "lenet"
 }
 
 
